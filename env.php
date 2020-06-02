b46d5561d17c5d64ce0fd73a4a0ad1034e263272
petarpejic97 <petar.pejic@outlook.com>
2020-06-02 23:50:04 +0200

    napravljena baza

--- env.php
+++ env.php
@@ -3,10 +3,10 @@
 namespace db;
 
 class DbConfig{
-    const HOST="localhost";
-    const USER = "root";
-    const PASS = "";
-    const DB ="zadace";
+    const HOST="eu-cdbr-west-03.cleardb.net";
+    const USER = "bd97b9f7b36e14";
+    const PASS = "c523d93e";
+    const DB ="heroku_fd1a3acf7f1c248";
 }
 
 ?>
